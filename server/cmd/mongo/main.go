86dee1161d35a8cf9b51a743035639e2698f1b14
yingning2021 <yingning2021@163.com>
2022-03-30 22:11:27 +0800

    feat: add tools to install grpc-gateway and mongodb demo

---
+++ server/cmd/mongo/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	c := context.Background()
+	mc, err := mongo.Connect(c, options.Client().ApplyURI("mongodb://localhost:27017/coolcar?readPreference=primary&ssl=false"))
+	if err != nil {
+		panic(err)
+	}
+	col := mc.Database("coolcar").Collection("account")
+	findRows(c, col)
+}
+
+func findRows(c context.Context, col *mongo.Collection) {
+	//res := col.Find(c, bson.M{})
+	res := col.FindOne(c, bson.M{
+		"open_id": "123",
+	})
+	fmt.Printf("%+v\n", res)
+	var row struct {
+		ID     primitive.ObjectID `bson:"_id"`
+		OpenID string             `bson:"open_id"`
+	}
+	err := res.Decode(&row)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", row)
+}
+
+func insertRows(c context.Context, col *mongo.Collection) {
+	res, err := col.InsertMany(c, []interface{}{
+		bson.M{
+			"open_id": "123",
+		},
+		bson.M{
+			"open_id": "456",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v", res)
+}
---
+++ tools/go.mod
@@ -0,0 +1,17 @@
+module tools
+
+go 1.18
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0
+	google.golang.org/protobuf v1.28.0
+)
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20200513103714-09dca8ec2884 // indirect
+	gopkg.in/yaml.v2 v2.2.3 // indirect
+)
