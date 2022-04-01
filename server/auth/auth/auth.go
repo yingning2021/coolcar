5c45b6299293b6742106eec35650c29199a2251a
yingning2021 <yingning2021@163.com>
2022-04-01 15:17:39 +0800

    feat: add jwt token gen

--- server/auth/auth/auth.go
+++ server/auth/auth/auth.go
@@ -7,17 +7,24 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"time"
 )
 
 //Service implement auth service
 type Service struct {
 	OpenIdResolver OpenIdResolver
 	Mongo          *dao.Mongo
+	TokenGenerator TokenGenerator
+	TokenExpire    time.Duration
 	Logger         *zap.Logger
 	authpb.UnimplementedAuthServiceServer
 }
 type OpenIdResolver interface {
 	Resolve(code string) (string, error)
+}
+
+type TokenGenerator interface {
+	GenerateToken(accountID string, expire time.Duration) (string, error)
 }
 
 func (s *Service) Login(c context.Context, request *authpb.LoginRequest) (*authpb.LoginResponse, error) {
@@ -31,9 +38,14 @@ func (s *Service) Login(c context.Context, request *authpb.LoginRequest) (*authp
 		s.Logger.Error("cannot resolve account id", zap.Error(err))
 		return nil, status.Error(codes.Internal, "")
 	}
+	tkn, err := s.TokenGenerator.GenerateToken(accountID, s.TokenExpire)
+	if err != nil {
+		s.Logger.Error("cannot generate token", zap.Error(err))
+		return nil, status.Error(codes.Internal, "")
+	}
 
 	return &authpb.LoginResponse{
-		AccessToken: "token for open id" + accountID,
-		ExpiresIn:   7200,
+		AccessToken: tkn,
+		ExpiresIn:   int32(s.TokenExpire.Seconds()),
 	}, nil
 }
